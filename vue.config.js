4154edead548982a1161776f45418dc12b3784f8
seogkiHong <skhong@eco.kr>
2020-07-22 16:44:56 +0900

    more stuff

--- vue.config.js
+++ vue.config.js
@@ -6,25 +6,41 @@ module.exports = {
       builderOptions: {
         appId: "electron-test-3",
         publish: "github",
-        dmg: {
-          contents: [
+        mac: {
+          target: [
             {
-              x: 110,
-              y: 150,
-            },
-            {
-              x: 240,
-              y: 150,
-              type: "link",
-              path: "/Applications",
+              target: "dmg",
+              contents: [
+                {
+                  x: 110,
+                  y: 150,
+                },
+                {
+                  x: 240,
+                  y: 150,
+                  type: "link",
+                  path: "/Applications",
+                },
+              ],
             },
           ],
         },
-        appImage: {
-          arch: ["armv7l", "arm64"],
+        linux: {
+          target: [
+            {
+              target: "appImage",
+              arch: ["armv7l", "arm64"],
+            },
+          ],
         },
-        nsis: {
-          createDesktopShortcut: "always",
+        win: {
+          target: [
+            {
+              target: "nsis",
+              arch: ["x64"],
+              createDesktopShortcut: "always",
+            },
+          ],
         },
       },
     },
