35119cd63c205e32d1fd252543dfefc32a2bb172
seogkiHong <skhong@eco.kr>
2020-07-22 16:06:35 +0900

    more stuff

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,31 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "electron-test-3",
+        publish: "github",
+        dmg: {
+          contents: [
+            {
+              x: 110,
+              y: 150,
+            },
+            {
+              x: 240,
+              y: 150,
+              type: "link",
+              path: "/Applications",
+            },
+          ],
+        },
+        appImage: {},
+        nsis: {
+          createDesktopShortcut: "always",
+        },
+        files: ["./build/**/*", "./src/background.js"],
+      },
+    },
+  },
 };
