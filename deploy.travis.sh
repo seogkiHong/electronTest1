7e0f2e516b6d2dd08b8e22e1201df7b532236f47
seogkiHong <skhong@eco.kr>
2020-07-23 09:40:03 +0900

    more stuff

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_OS_NAME" == osx ]; then
     yarn release
 else
     # deploy on windows and linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "sudo apt-get update && sudo apt-get install snapd && sudo snap install snapcraft --classic && yarn --link-duplicates --pure-lockfile && yarn release --linux AppImage snap --win"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux AppImage snap --win"
 fi 
